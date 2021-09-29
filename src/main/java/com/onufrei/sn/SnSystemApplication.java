0085b68f63864e93ec2fea23f4b3aca4ee46eb78
Artem Onufrei <2hilderoy2@gmail.com>
2021-09-29 11:55:17 +0300

    Fixed imports

--- src/main/java/com/onufrei/sn/SnSystemApplication.java
+++ src/main/java/com/onufrei/sn/SnSystemApplication.java
@@ -1,15 +1,8 @@
 package com.onufrei.sn;
 
-import com.onufrei.sn.dto.AccountDto;
-import com.onufrei.sn.model.Account;
-import com.onufrei.sn.service.impl.AccountServiceImpl;
-import com.onufrei.sn.service.intf.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.UUID;
 
 @SpringBootApplication
 public class SnSystemApplication implements CommandLineRunner {
